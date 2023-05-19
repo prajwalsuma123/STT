4b6c1251219db62586abedeb8b01f11f20be1afa
prajwal.sonawane <prajwal.sonawane@sumasoft.net>
2023-05-19 17:42:49 +0530

    remove unused file for client

--- src/main/java/com/sumasoft/stt/SpeachToTextApplication.java
+++ src/main/java/com/sumasoft/stt/SpeachToTextApplication.java
@@ -1,10 +1,5 @@
 package com.sumasoft.stt;
 
-import com.sumasoft.stt.audio.AudioConfig;
-import com.sumasoft.stt.audio.AudioFile;
-import com.sumasoft.stt.audio.Microphone;
-import com.sumasoft.stt.config.AppConfig;
-import com.sumasoft.stt.user.AunthenticateUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -17,34 +12,17 @@ import java.net.URISyntaxException;
 import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {SpeachToTextApplication.class,AppConfig.class})
+@ComponentScan(basePackageClasses = {SpeachToTextApplication.class})
 public class SpeachToTextApplication {
 
 	public static final Logger logger= LoggerFactory.getLogger(SpeachToTextApplication.class);
-	
+
 	public static void main(String[] args) throws IOException, URISyntaxException, NoSuchAlgorithmException, LineUnavailableException, InterruptedException {
 		ApplicationContext applicationContext= SpringApplication.run(SpeachToTextApplication.class, args);
 		getSysteminfo();
 		applicationContext.getEnvironment().getProperty("myapp.name");
-		AunthenticateUser aunthenticateUser=new AunthenticateUser();
 
-		if(aunthenticateUser.aunthenticateUser("98d0c598-eb03-11ed-a05b-0242ac120003")){
-			logger.info("User Authenticated Sucessfully");
-
-//			Microphone microphone=new Microphone();
-//			microphone.startMicrophone();
-			try
-			{
-				AudioFile audioFile=new AudioFile();
-				audioFile.sendAudio();
-			}
-			catch (Exception e){
-				
-			}
-			
-		
-		}
-
+		logger.info("User Authenticated Sucessfully");
 	}
 
 	private static void getSysteminfo() {
--- src/main/java/com/sumasoft/stt/client/AudioClient.java
+++ src/main/java/com/sumasoft/stt/client/AudioClient.java
@@ -1,6 +1,5 @@
 package com.sumasoft.stt.client;
 
-import com.sumasoft.stt.response.Response;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 
