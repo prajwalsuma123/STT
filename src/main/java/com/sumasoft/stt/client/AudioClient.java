05f9b849d9c61075f3c6e8d2c3ab714a8eeca450
prajwal.sonawane <prajwal.sonawane@sumasoft.net>
2023-05-31 14:55:22 +0530

    remove AudioFile and And AudioClient

--- src/main/java/com/sumasoft/stt/client/AudioClient.java
+++ src/main/java/com/sumasoft/stt/client/AudioClient.java
@@ -1,27 +1,24 @@
 package com.sumasoft.stt.client;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sumasoft.stt.result.ResultNotifiable;
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.handshake.ServerHandshake;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.net.URI;
-import java.util.TreeSet;
 
 public class AudioClient extends WebSocketClient {
     public static final Logger logger= LoggerFactory.getLogger(AudioClient.class);
     ResultNotifiable resultNotifiable;
-    JsonParser parser;
-    TreeSet<String> treeSet;
-    JsonObject jsonObject;
-
+    ObjectMapper objectMapper;
     public AudioClient(URI serverUri, ResultNotifiable resultNotifiable) {
         super(serverUri);
         this.resultNotifiable=resultNotifiable;
-        this. parser= new JsonParser();
+        this.objectMapper= new ObjectMapper();
     }
 
 
@@ -32,39 +29,29 @@ public class AudioClient extends WebSocketClient {
 
     @Override
     public void onMessage(String s) {
-        JsonElement rootElement = parser.parse(s);
-        jsonObject=rootElement.getAsJsonObject();
-        if(jsonObject.has("partial")){
-            this.resultNotifiable.partialText(s);
-        } else if (jsonObject.has("text")) {
-            this.resultNotifiable.resultText(s);
-        }
-    }
 
-    /**
-     * fliter json object and get value as per requirement
-     * for e.g. if any user want only resultText not partialtext and visevarsa
-     * @param jsonObject
-     */
-    private void filterPartialAndResult(JsonObject jsonObject) {
-        if(jsonObject.has("partial")){
-            //send value present inside key
-            this.resultNotifiable.partialText(jsonObject.get("partial").getAsString());
-        }
-        if(jsonObject.has("text")){
-            //send value present inside key
-            this.resultNotifiable.resultText(jsonObject.get("text").getAsString());
+        try {
+            JsonNode jsonNode = objectMapper.readTree(s);
+            jsonNode.has("result");
+            if(jsonNode.has("result")){
+                this.resultNotifiable.resultText(s);
+            }
+            else {
+                this.resultNotifiable.partialText(s);
+
+            }
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
         }
     }
 
     @Override
     public void onClose(int i, String s, boolean b) {
-        logger.info("Connection closed");
+
     }
 
     @Override
     public void onError(Exception e) {
-        logger.info("Error Occured in library");
+        logger.info("Error occured while speech-to-Text");
     }
-
 }
